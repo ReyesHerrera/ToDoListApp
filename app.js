9b8d57033ca42fdb32da4b2389209825ff84c737
Howard Kuo <iamhowardkuo@gmail.com>
2019-02-26 21:37:45 -0800

    Connect to mongo db but no schema yet

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+//DB Config
+var db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+mongoose.connect(db,{ useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
