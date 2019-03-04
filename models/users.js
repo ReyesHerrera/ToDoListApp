d1d2dbd81140447b80b876f2ee111a17f9babe28
deyanira <30326911+miscdats@users.noreply.github.com>
2019-03-03 16:57:11 -0800

    create user schema

---
+++ models/users.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+var moment = require('moment');
+var bcrypt = require('bcrypt');
+
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  createdDate: {
+    type: String,
+    default: moment(new Date()).format("MMM DD, YYYY") //"Day, 5PM 18"
+  },
+  tasks: {
+    type: ObjectId,
+    ref: 'Task'
+  }
+});
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
