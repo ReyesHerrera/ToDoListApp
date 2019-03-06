2ad09c5430544f9c31d3ff069e4b9be3e1e00952
ReyesHerrera <rhperla.y@gmail.com>
2019-03-05 18:47:55 -0800

    adding some changes to help with merge

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//might change to constant these variables
+//import node libraries
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,41 +6,36 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
-var session = require('express-session');
-//var passport = require('passport');
+var mongoose = require('mongoose');
 
-//using mongodb for data base
-const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/ToDoListApp');
-let db = mongoose.connection;
+// req schemas from models dir
+var tasksModel = require('./models/tasks');
+var usersModel = require('./models/users');
 
-//checking connection to server
-db.once('open', function(){
-    console.log('We are connected to MongoDB');
-});
-//checking for DB errors
-db.on('error', function(err){
-    console.log(err);
-});
-
-
-//might have to consolidate into one file in routes
-//var indexRouter = require('./routes/index');
+//require modules from routes dir
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var navbarRouter = require('./routes/navbar');
+var tasksRouter = require('./routes/tasks');
+var tasksViewRouter = require('./routes/tasksView');
 
-//inititalizing
+//create app
 var app = express();
 
-//bringing models for Tasks
-let Tasks = require('./models/tasks');
+//DB Config
+// var db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+// mongoose.connect(db,
+//   { useNewUrlParser: true })
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.log(err));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//add middleware libraries
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -51,148 +46,14 @@ app.use(bodyParser.json())
 
 //Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//Express Session Middleware
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true
-}));
 
-// Express Messages Middleware
-app.use(require('connect-flash')());
-app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
-  next();
-});
-
-// Express Validator Middleware
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
-
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-
-//view engine setup
-//app.use('/', indexRouter);
+//route-handling code
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/navbar', navbarRouter);
-//HOME ROUTE
-app.get('/', function(req, res) {
-  Tasks.find({}, function(err, tasks){
-    if(err){
-      console.log(err);
-    }else {
-      res.render('index', {
-        title: 'ToDoList App',
-        tasks: tasks
-    });
-  }
-  });
-});
-
-//GET one task
-app.get('/task/:id', function(req, res){
-  Task.findById(req.params.id, function(err, task){
-    res.render('task',{
-      task:task
-    });
-  });
-});
-//router for add
-app.get('/tasks/add', function(req, res, next) {
-  res.render('addTask', {
-    title: 'ToDoList App  Add'
-  });
-});
-
-
-//post route for add submit form
-app.post('/tasks/add', function(req, res){
-  req.checkBody('taskName', 'task name is required').notEmpty();
-  req.checkBody('priority', 'priority  is required').notEmpty();
-  req.checkBody('content', 'content is required').notEmpty();
-  req.checkBody('duedate', 'due date is required').notEmpty();
-
-  //Error for validations
-  let errors = req.validationErrors();
-  if (errors){
-    res.render('addTask',{
-      title: 'Add Task',
-      errors:errors
-    });
-  }else{
-  let tasks = new Tasks();
-  tasks.taskName = req.body.taskName;
-  tasks.priority = req.body.priority;
-  tasks.content = req.body.content;
-  tasks.duedate = req.body.duedate;
-  //console.log('Its working');
-  //return;
-
-  tasks.save(function(err){
-    if(err){
-      console.log(err);
-      return;
-    } else {
-      req.flash('success', 'Task was added');
-      res.redirect('/');
-    }
-  });
-};
-
-//Edit form for task
-app.get('/task/edit/:id', function(req, res){
-  Task.findById(req.params.id, function(err, task){
-    res.render('updateTask',{
-      title:'updateTask',
-      task:task
-    });
-  });
-});
-
-//POST route for update submit form
-app.post('/tasks/edit/:id', function(req, res){
-  let tasks = {};
-  tasks.taskName = req.body.taskName;
-  tasks.priority = req.body.priority;
-  tasks.content = req.body.content;
-  tasks.duedate = req.body.duedate;
-
-  let query = {_id:req.params.id}
-
-  Tasks.update(query, tasks, function(err){
-    if(err){
-      console.log(err);
-      return;
-    } else {
-      req.flash('success', 'Task was updated');
-      res.redirect('/');
-      }
-    });
-  });
-  app.delete('/tasks/:id', function(rep, res){
-    let query = {_id:req.params.id}
-
-    Tasks.remove(query, function(err){
-      if(err){
-        console.log(err);
-      }
-      res.send('successfull deletion');
-    });
-  });
-});
+app.use('/tasks', tasksRouter);
+app.use('/tasksView', tasksViewRouter);
 
 
 // catch 404 and forward to error handler
--- models/tasks.js
+++ models/tasks.js
@@ -1,23 +1,39 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
-//create database structure for tasks table
-let tasksSchema = mongoose.Schema({
-    taskName:{
-        type: String,
-        require: true
-    },
-    priority:{
-        type: String,
-        require: true
-    },
-    content:{
-        type: String,
-        require: true
-    },
-    duedate:{
-        type: String,
-        require: true
-    },
+var moment = require('moment');
+var now = moment(new Date()).format("MMM DD, YYYY");
+
+
+const TaskSchema = new Schema({
+  taskAuthor: {
+    type: ObjectId,
+    ref: "User"
+  },
+  taskName: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  taskDateEntered: {
+    type: String,
+    default: now //"Sun, 3PM"
+  },
+  taskDateDue: {
+    type: String,
+    default: moment().add(7, 'days').format("MMM DD, YYYY") // add 7 days from now
+  },
+  taskContent: {
+    type: String,
+    trim: true,
+    default: "No description provided."
+  },
+  priority: {
+    type: String,
+    require: true
+  }
 });
 
-let Tasks = module.exports = mongoose.model('Tasks', tasksSchema);
+var Task = mongoose.model('Task', TaskSchema);
+module.exports = Task;
